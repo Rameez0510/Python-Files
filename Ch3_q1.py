c89eba95dc6bea802cad7493109e20f0a07d57ec
Rameez0510 <93828170+Rameez0510@users.noreply.github.com>
2021-11-06 22:00:11 +0530

    Create Ch3_q1.py

---
+++ Ch3_q1.py
@@ -0,0 +1,5 @@
+# Q. 1 Write a python program to display user entered name followed by good afternoon using input function ()
+
+ga = "Good afternoon! "
+name = input ("What iz ur name ")
+print(ga + name)
