c42dce039f518edc64dfa05f978e653966b7ca52
Rameez0510 <93828170+Rameez0510@users.noreply.github.com>
2021-11-06 20:32:57 +0530

    Create Escape_sequence_characters.py

---
+++ Escape_sequence_characters.py
@@ -0,0 +1,13 @@
+# Escape sequence characters
+
+# \n
+print("My name iz Rameez Khan \n I live in Bhopal")
+
+# \t
+print ("Hello \t everyone")
+
+# \'
+print ("hello \' guys")
+
+# \\
+print ("are you a boy\\girl ? ")
