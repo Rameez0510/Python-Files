a268e1ff13d78d51dceec7b09182dd5979118018
Rameez0510 <93828170+Rameez0510@users.noreply.github.com>
2021-11-06 22:36:18 +0530

    Create Ch3_q3_q4.py

---
+++ Ch3_q3_q4.py
@@ -0,0 +1,11 @@
+# Q. 3 and Q. 4
+
+print ("This bot will detect the double space in a sentence")
+
+sentence = input ("Say something bhai! \n")
+print (sentence.find("  "))
+
+#Replacing the double space to single
+
+sentence = sentence.replace ("  " , " ")
+print (sentence)
