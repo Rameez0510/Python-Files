4e61397ca6520ed84696cee8acd696c79743927a
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-09 20:15:24 +0530

    Create List Methods.py

---
+++ Notes/List Methods.py
@@ -0,0 +1,22 @@
+# List methods
+
+l = [1, 8,7,2,21,15]
+
+
+l.sort() # sort in ascending order
+print (l)
+
+l.reverse() #reverse the order
+print (l)
+
+l.append(8) # adds the number at the end of the list
+print (l)
+
+l.insert(3, 8) # adds a number on a chosen index place 
+print(l)
+
+l.pop(6) # removes the number from the index place
+print (l)
+
+l.remove (21) # removes a mentioned number
+print (l)
