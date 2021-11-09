87168ae9b2877f6d8c45d04e8ed6168c2fcfcbcc
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-09 22:36:00 +0530

    Create Store fruits.py

---
+++ Practice Question/Chapter 4/Store fruits.py
@@ -0,0 +1,12 @@
+# Store list of fruits
+
+a = input ("Enter fruit a \n")
+b = input ("Enter fruit b \n")
+c = input ("Enter fruit c \n")
+d = input ("Enter fruit d \n")
+e = input ("Enter fruit e \n")
+f = input ("Enter fruit f \n")
+g = input ("Enter fruit g \n")
+
+fruits = [ a, b, c, d, e, f, g]
+print(fruits)
