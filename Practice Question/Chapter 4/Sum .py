18729ac1667b8ba18ae3bccb8a0797a9f86eeea6
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-09 22:49:25 +0530

    Create Sum .py

---
+++ Practice Question/Chapter 4/Sum .py
@@ -0,0 +1,11 @@
+# list of number and their sum
+
+a = int(input("Enter first number\n"))
+b = int(input("Enter second number\n"))
+c = int(input("Enter third number\n"))
+d = int(input("Enter fourth number\n"))
+
+number = [ a, b, c, d]
+print(number)
+
+print(number[0] + number[1] + number[2] + number[3])
