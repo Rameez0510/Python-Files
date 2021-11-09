d994ff83fcae15c7f5c4d949a60198ac637867ad
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-09 19:06:25 +0530

    Create List.py

---
+++ Notes/List.py
@@ -0,0 +1,16 @@
+#Lists
+
+
+# adding multiple values in a single variable
+info = ["Rameez" , "Bhopal" , 17 , 28.3]
+print (info)
+
+# changing the value
+info[1] = "Ghoda Nakkas"
+print(info)
+
+info[2] += 10
+print (info)
+
+# printing a specific part
+print(info[3])
