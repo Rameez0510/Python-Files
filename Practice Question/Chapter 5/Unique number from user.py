ab13689e959fabfe160e02ce5449943d11278010
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-10 23:18:53 +0530

    Create Unique number from user.py

---
+++ Practice Question/Chapter 5/Unique number from user.py
@@ -0,0 +1,13 @@
+a = input ("Enter first number \n")
+b = input ("Enter second number \n")
+c = input ("Enter third number \n")
+d = input ("Enter forth number \n")
+e = input ("Enter fifth number \n")
+f = input ("Enter sixth number \n")
+g = input ("Enter seventh number \n")
+h = input ("Enter eighth number \n")
+
+numb = {
+    a, b, c, d, e, f, g, h
+}
+print(numb)
