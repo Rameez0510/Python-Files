ca00bd19a7a76c41d401c3f0881a729ea65bd885
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-10 23:38:15 +0530

    Create Ability.py

---
+++ Practice Question/Chapter 5/Ability.py
@@ -0,0 +1,16 @@
+D = { }
+a = input ("Enter name \n")
+b = input ("Enter ability \n")
+c = input ("Enter name \n")
+d = input ("Enter ability \n")
+e = input ("Enter name \n")
+f = input ("Enter ability \n")
+g = input ("Enter name \n")
+h = input ("Enter ability \n")
+D.update({
+    a : b, 
+    c : d, 
+    e : f, 
+    g : h
+})
+print(D)
