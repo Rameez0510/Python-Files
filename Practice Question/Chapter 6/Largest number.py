a6009a295e8cb457936c78b0d9f5f7108a382d7d
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-11 15:05:36 +0530

    Create Largest number.py

---
+++ Practice Question/Chapter 6/Largest number.py
@@ -0,0 +1,18 @@
+a = int(input("Enter number \n"))
+b = int(input("Enter number \n"))
+c = int(input("Enter number \n"))
+d = int(input("Enter number \n"))
+
+if (a > b) :
+	x = a
+else :
+	x = b
+if (c > d) :
+	y = c
+else :
+	y = d
+	
+if (x > y) :
+	print(x , " is the largest number")
+else :
+	print(y ,  " is the largest number")
