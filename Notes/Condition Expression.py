7f587807f84486f1e88a40f176e6d1c34b6c16d3
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-11 11:31:19 +0530

    Create Condition Expression.py

---
+++ Notes/Condition Expression.py
@@ -0,0 +1,21 @@
+# conditional expression
+
+a = 10
+if(a<5) : # if a<5 this will execute
+    print("a is greater than 5") 
+elif(a>9) : # if condition 1 is false this will execute
+    print("a is greater than 9")
+else : # if all above conditions are false then this will execute
+    print("10 is not greater")
+
+# start and end
+if(a>1) :
+    print ("A is greater") # starts a new when if iz written
+else :
+    print ("nothing iz greater") # ends at else
+ 
+ 
+if (a<20) : # new started
+    print (a, "iz smaller than 20")
+if (a<=10) : # above one ends their and new one started
+    print("a iz equal or smaller than 10")
