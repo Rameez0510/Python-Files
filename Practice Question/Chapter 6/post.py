1dee2f09831fab9b83b316de9206ec72935ad252
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-12 12:15:05 +0530

    Add files via upload

---
+++ Practice Question/Chapter 6/post.py
@@ -0,0 +1,6 @@
+# check whether the post is talking about rameez or not
+post = input("Enter your post : \n")
+if "rameez" in post :
+    print("yes you are talking about rameez")
+else :
+    print("no you are not talking about rameez")
