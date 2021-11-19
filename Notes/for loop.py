38af00aaa9ead2434325a8a9c2e8146f7bf24f97
Rameez Khan <93828170+Rameez0510@users.noreply.github.com>
2021-11-19 11:42:10 +0530

    Add files via upload

---
+++ Notes/for loop.py
@@ -0,0 +1,8 @@
+# for loop
+fruits = ['apple' , 'mango' , 'banana' , 'grapes' , 'peach' , 'watermelon' , 'pineapple']
+for i in fruits :
+    print(i)
+
+a = "rameez"
+for b in a :
+    print(b)
---
+++ Notes/while loop.py
@@ -0,0 +1,7 @@
+# while loop
+
+i = 2
+while  i < 20 : # execute until the statement becomes false
+    print("yes" ,i)
+    i += 1 # increase the value of i and execute again
+    
